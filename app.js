1413ed65545602b4acc6aa51c46544e3b8b36859
godlaughu <godlaughu@qq.com>
2019-01-21 09:30:56 +0800

    0121integration1
    部分整合

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+  onLaunch() {
+    let openId, nickName;
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+            appid: 'wxbaa12aa38c7c01b9',
+            secret: '1a12fa9bec9ef347b9553eec44042500',
+            js_code: res.code,
+            grant_type: 'authorization_code'
+          },
+          method: 'GET',
+          success: res => {
+            openId = res.data.openid;
+            wx.getSetting({ // 获取用户信息
+              success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                  wx.getUserInfo({
+                    success: res => {
+                      nickName = res.rawData.nickName;
+                      if (this.userInfoReadyCallback) {
+                        this.userInfoReadyCallback(res)
+                      }
+                      // wx.request({
+                      //   url: 'http://127.0.0.1:3200/',
+                      //   // method: 'GET',
+                      //   method: 'POST',
+                      //   data: {,,,asdssssss
+                      //     openId,
+                      //     nickName
+                      //   },
+                      //   header: {
+                      //     'content-type': 'application/json' // 默认值
+                      //   },
+                      //   success: (res) => {
+                      //     console.log(res);
+                      //   }
+                      // })
+                    }
+                  })
+                }
               }
-            }
-          })
-        }
+            })
+            // wx.request({
+            //   url: 'http://127.0.0.1:3200/',
+            //   method: 'POST',
+            //   data: {
+            //     openid: res.data.openid
+            //   },
+            //   header: {
+            //     'content-type': 'application/json' // 默认值
+            //   },
+            //   success: (res) => {
+            //     console.log(res);
+            //   }
+            // })
+          }
+        })
       }
     })
   },
